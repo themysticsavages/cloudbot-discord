aaf10f3bc43eb632e01b0b8098c23b9425646d0f
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-05-05 07:57:27 -0400

    please don't ask

--- run.cmd
+++ run.cmd
@@ -1,8 +1,17 @@
 @echo off
-echo This will replace the text in bot.js and run the file. Continue?
+
+echo Replaces text in bot.js and runs it. Continue?
 pause
+
 cls
+
+cd etc
 py replace.py
+
+cls
+
 pause
 cls
+
+cd ..
 node bot.js
