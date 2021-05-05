f05726e9234f9e166c2ba467b519fee42eaf5979
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-05-05 08:26:46 -0400

    definitely changed

--- bin/run.sh
+++ bin/run.sh
@@ -7,12 +7,14 @@ function pause() {
 echo 'This will replace the text in node.js and run it.'
 pause 'Continue?' 
 
+cd ..
 cd etc
+
 clear
 py replace.py
 
 pause 'Continue?'
 cd ..
-clear
 
+clear
 node bot
