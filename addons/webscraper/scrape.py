da94d3e170baf0830962e13f2b72ebd0f9b58216
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-05-16 13:00:58 -0700

    removed unnecessary chdir

--- addons/webscraper/scrape.py
+++ addons/webscraper/scrape.py
@@ -1,9 +1,6 @@
 from bs4 import BeautifulSoup
 import requests, sys, os
 
-local = '\\'.join(__file__.split('\\')[0:-1])
-os.chdir(local)
-
 page = requests.get("https://bing.com/search?q="+sys.argv[1])
 soup = BeautifulSoup(page.content, 'html.parser')
 list(soup.children)
