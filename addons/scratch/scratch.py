0264b5eb9ba410f647cdf7a38cc5d19980dc82dd
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-05-30 12:22:21 -0400

    updated for erros

--- addons/scratch/scratch.py
+++ addons/scratch/scratch.py
@@ -7,15 +7,18 @@ res = requests.request('GET', 'https://api.scratch.mit.edu/users/'+user+'/')
 JSON = json.loads(res.text)
 results = []
 
-join = str(JSON['history']['joined']).split('T', 1)[0]
-image = str(JSON['profile']['images']['50x50'])
-stats = str(JSON['profile']['bio']).replace('\n\n', '')
+try:
+    join = str(JSON['history']['joined']).split('T', 1)[0]
+    image = str(JSON['profile']['images']['50x50'])
+    stats = str(JSON['profile']['bio']).replace('\n\n', '')
 
-results.append(join)
-results.append(image)
-results.append(stats)
-
-results = str(results).replace('[', '').replace(']', '').replace("'", '').rstrip('\n')
-
-print(results)
-sys.stdout.flush()
+    results.append(join)
+    results.append(image)
+    results.append(stats)
+    
+    results = str(results).replace('[', '').replace(']', '').replace("'", '').rstrip('\n')
+    print(results)
+    sys.stdout.flush()
+except KeyError:
+    print('`Failed to find '+user+'. Please look for someone else.`')
+    sys.stdout.flush()
