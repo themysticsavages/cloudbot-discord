48e7bb317c9cc34fcc72eff8897b66a18a61a40c
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-06-04 09:13:44 -0400

    changed from bing to yahoo
    cRy AbOuT iT

--- addons/webscraper/webscraper.py
+++ addons/webscraper/webscraper.py
@@ -1,11 +1,23 @@
-from bs4 import BeautifulSoup
-import requests, sys
+import requests
+import bs4
+import sys
 
-page = requests.get("https://bing.com/search?q="+sys.argv[1])
-soup = BeautifulSoup(page.content, 'html.parser')
-list(soup.children)
+text = sys.argv[1]
 
-content = soup.find_all('p')[0].get_text()
+url = 'https://search.yahoo.com/search?p='+text+'&vm=r'
+request_result = requests.get( url )
+  
+soup = bs4.BeautifulSoup(request_result.text,
+                         "html.parser")
 
-print(content)
-sys.stdout.flush()
+obj = []
+heading_object=soup.find_all('p', class_ = 'fz-ms lh-1_43x') 
+for info in heading_object:
+    obj.append(info.getText())
+
+try:
+  print(obj[0])
+  sys.stdout.flush()
+except IndexError:
+  print("`Failed to find "+text+". There may be a good chance you just typed gibberish.`")
+  sys.stdout.flush()
