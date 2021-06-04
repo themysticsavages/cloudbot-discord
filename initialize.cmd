0d1c1edeb647021f3e0ecc89bf5cddd758d92b7f
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-06-04 10:22:08 -0700

    initialize

--- initialize.cmd
+++ initialize.cmd
@@ -5,25 +5,23 @@ echo 1. Download bot.js
 echo 2. Install required packages
 echo 3. Run bot.js
 echo ----------------------------
-echo.
 
-set /p opt='> '
+set /p opt=!
 
-:1
-cls
-curl --output bot.new.js https://raw.githubusercontent.com/themysticsavages/cloudbot-discord/main/bot.js
+if %opt% == 1 goto download
+if %opt% == 2 goto install
+if %opt% == 3 goto run
+
+:download
+curl --output bot.new https://raw.githubusercontent.com/themysticsavages/cloudbot-discord/main/bot.js
 exit
 
-:2
-cls
-npm i
+:install
 py -m pip install -r requirements.txt
+npm i
+exit
 
-:3
+:run
 cls
-node bot.js
+node bot
 pause
-
-if %opt%==1 goto 1
-if %opt%==2 goto 2
-if %opt%==3 goto 3
