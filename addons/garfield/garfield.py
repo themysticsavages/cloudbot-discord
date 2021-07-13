c2226a30fa82af474899c728dd78a3b859245b3b
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-07-13 07:03:53 -0700

    New Garfield comic querying!

--- addons/garfield/garfield.py
+++ addons/garfield/garfield.py
@@ -1,12 +1,21 @@
+from bs4 import BeautifulSoup
 import requests
+import sys
 import os
-import re
 
 os.chdir('\\'.join(__file__.split('\\')[0:-1]))
+comicdate = sys.argv[1].split('.')
 
-res = requests.get('https://www.mezzacotta.net/garfield/')
-num = re.findall(r'\d+', res.text)[28]
+r = requests.get('https://ermel.org/garfield.php?day={}&month={}&year={}'.format(comicdate[0], comicdate[1], comicdate[2]))
+soup = BeautifulSoup(r.text, 'html.parser')
+img = soup.findAll('img')[0]
 
-res = requests.get(f'https://www.mezzacotta.net/garfield/comics/{num}.png')
-with open('comic.png', 'wb') as png:
-    png.write(res.content)
+r = requests.get(img['src'], stream=True)
+soup = BeautifulSoup(r.text, 'html.parser')
+
+if r.status_code == 200:
+    with open('comic.png', 'wb') as f:
+        for chunk in r.iter_content(1024):
+            f.write(chunk)
+else:
+    print('That comic was not found! Did you use a correct date with the DD-MM-YYYY format?')
